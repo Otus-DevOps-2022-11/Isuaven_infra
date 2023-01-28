a11d93de170133bac22da916e46dfbbe37a94d4d
Isuaven <Isuaven@users.noreply.github.com>
2023-01-28 15:13:13 +0300

    Changed required_providers for old TravisCI tf version compatibility

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,11 +1,16 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_providers {
+    yandex = "~> 0.35.0"
+  }
+  required_version = ">= 0.12.0"
+/*
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
       version = ">= 0.44.0"
     }
   }
+*/
 }
 
 resource "yandex_vpc_network" "app-network" {
