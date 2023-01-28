f9de4960ffb26d39c2cbd0561f9fb8da32e5d11f
Isuaven <Isuaven@users.noreply.github.com>
2023-01-28 23:53:38 +0300

    Completed ansible-1

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,16 +1,17 @@
 terraform {
+/*
   required_providers {
     yandex = "~> 0.35.0"
   }
   required_version = ">= 0.12.0"
-/*
+*/
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
       version = ">= 0.44.0"
     }
   }
-*/
+
 }
 
 provider "yandex" {
