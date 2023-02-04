9343dba8bfbf34ee4dc04492f2140139ee892c1e
Isuaven <Isuaven@users.noreply.github.com>
2023-02-04 18:00:36 +0000

    Fixed action errors v3

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -5,8 +5,8 @@ terraform {
     region   = "ru-central1"
     key      = "terraform/stage/terraform.tfstate"
     # Fake credentionals:
-    access_key = "inKfymJWcMa8XzFtvoOA"
-    secret_key = "xEMiI4A4dwmzJuE66yjC"
+    access_key = "YCAJEzxDhimRFlx_kYElbzPnH"
+    secret_key = "YCM2iB_e9qa-IxIYFmBsRNiIYz__CRmGlZpmCagl"
 
     skip_region_validation      = true
     skip_credentials_validation = true
